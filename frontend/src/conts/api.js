afa60616c99814732bcecfff17b7692db7d8a837
unknown <jun16love@naver.com>
2023-01-31 18:09:55 +0900

    프론트 수정

--- frontend/src/conts/api.js
+++ frontend/src/conts/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const HOST_URL = 'localhost:8080'
+const HOST_URL = 'http://localhost:8082'
 export function POST_API(file) {
     console.log(`업로드 파일 : ${file}`)
-    return
     axios({
         method: 'post',
-        url: HOST_URL,
+        url: `${HOST_URL}/save`,
         data: {
             file
         }
