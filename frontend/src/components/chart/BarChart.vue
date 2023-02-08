8d604504b674b30160c00cd906c2fbe460eb8b6d
jacob <53252566+jasonlee-martina@users.noreply.github.com>
2023-02-08 14:11:22 +0900

    프론트엔드 : 차트 조회 수정완

--- frontend/src/components/chart/BarChart.vue
+++ frontend/src/components/chart/BarChart.vue
@@ -14,59 +14,95 @@ export default {
     props : {
         propChartData : Array
     },
-    data: () => ({
-      chartData : null
-    }),
-    mounted() {
-        let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
-            
-        chart.legend = new am4charts.Legend()
-        chart.legend.position = 'top'
-        chart.legend.paddingBottom = 20
-        chart.legend.labels.template.maxWidth = 95
+    /* AS - IS */
+    // mounted() {
+    //     console.log(this.propChartData)
+    //     this.chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
+    // },
+    // beforeUpdate() {
+    //     this.chart.legend = new am4charts.Legend()
+    //     this.chart.legend.position = 'top'
+    //     this.chart.legend.paddingBottom = 20
+    //     this.chart.legend.labels.template.maxWidth = 95
 
-        let xAxis = chart.xAxes.push(new am4charts.CategoryAxis())
-        xAxis.dataFields.category = 'sub_sta_nm'
-        xAxis.renderer.cellStartLocation = 0.1
-        xAxis.renderer.cellEndLocation = 0.9
-        xAxis.renderer.grid.template.location = 0;
+    //     let xAxis = this.chart.xAxes.push(new am4charts.CategoryAxis())
+    //     xAxis.dataFields.category = 'sub_sta_nm'
+    //     xAxis.renderer.cellStartLocation = 0.1
+    //     xAxis.renderer.cellEndLocation = 0.9
+    //     xAxis.renderer.grid.template.location = 0;
 
-        let yAxis = chart.yAxes.push(new am4charts.ValueAxis());
-        yAxis.min = 0;
+    //     let yAxis = this.chart.yAxes.push(new am4charts.ValueAxis());
+    //     yAxis.min = 0;
 
-        function createSeries(value, name) {
-            let series = chart.series.push(new am4charts.ColumnSeries())
-            series.dataFields.valueY = value
-            series.dataFields.categoryX = 'sub_sta_nm'
-            series.name = name
+    //     function createSeries(value, name) {
+    //         let series = this.chart.series.push(new am4charts.ColumnSeries())
+    //         series.dataFields.valueY = value
+    //         series.dataFields.categoryX = 'sub_sta_nm'
+    //         series.name = name
 
-            let bullet = series.bullets.push(new am4charts.LabelBullet())
-            bullet.interactionsEnabled = false
-            bullet.dy = 30;
-            bullet.label.text = '{valueY}'
-            bullet.label.fill = am4core.color('#ffffff')
+    //         let bullet = series.bullets.push(new am4charts.LabelBullet())
+    //         bullet.interactionsEnabled = false
+    //         bullet.dy = 30;
+    //         bullet.label.text = '{valueY}'
+    //         bullet.label.fill = am4core.color('#ffffff')
 
-            return series;
+    //         return series;
+    //     }
+
+    //     createSeries('ride_pasgr_num', '승차객 수');
+    //     createSeries('alight_pasgr_num', '하차객 수');
+
+    //     this.chart.data = this.chartData
+    // },
+    methods : {
+        /* TO-BE */
+        renderChart(val) {
+            let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
+
+            chart.legend = new am4charts.Legend()
+            chart.legend.position = 'top'
+            chart.legend.paddingBottom = 20
+            chart.legend.labels.template.maxWidth = 95
+
+            let xAxis = chart.xAxes.push(new am4charts.CategoryAxis())
+            xAxis.dataFields.category = 'sub_sta_nm'
+            xAxis.renderer.cellStartLocation = 0.1
+            xAxis.renderer.cellEndLocation = 0.9
+            xAxis.renderer.grid.template.location = 0;
+
+            let yAxis = chart.yAxes.push(new am4charts.ValueAxis());
+            yAxis.min = 0;
+
+            function createSeries(value, name) {
+                let series = chart.series.push(new am4charts.ColumnSeries())
+                series.dataFields.valueY = value
+                series.dataFields.categoryX = 'sub_sta_nm'
+                series.name = name
+
+                let bullet = series.bullets.push(new am4charts.LabelBullet())
+                bullet.interactionsEnabled = false
+                bullet.dy = 30;
+                bullet.label.text = '{valueY}'
+                bullet.label.fill = am4core.color('#ffffff')
+
+                return series;
+            }
+
+            createSeries('ride_pasgr_num', '승차객 수');
+            createSeries('alight_pasgr_num', '하차객 수');
+
+            chart.data = val
         }
-
-        createSeries('ride_pasgr_num', '승차객 수');
-        createSeries('alight_pasgr_num', '하차객 수');
-
-        chart.data = this.chartData
-        
-    },
-    updated() {
-        console.log('!!!')
-        // let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
-        // chart.data = this.$props.propChartData
-    },
+    }, 
     /* 
         computed VS watch
+        computed : 반응형 getter
+        watch    : 반응형 callback
     */
     watch : {
         propChartData() {
-            this.chartData = this.propChartData
-            console.log(this.chartData)
+            // this.chartData = this.$props.propChartData
+            return this.renderChart(this.$props.propChartData)
         }
     }
     // beforeDestroy() {
